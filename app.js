ce902b55d210fa0b13d8c3f1224d33765e27cb47
= <=>
2023-05-03 15:48:44 +0200

    {feat: add controls} finish project

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ previewImg = uploadBox.querySelector('img'),
 fileInput = uploadBox.querySelector('input'),
 widthInput = document.querySelector('.width input'),
 heightInput = document.querySelector('.height input'),
-ratioInput = document.querySelector('.ratio input');
+ratioInput = document.querySelector('.ratio input'),
+qualityInput = document.querySelector('.quality input'),
+downloadBtn = document.querySelector('.download-btn');
 
 let ogImageRatio;
 
@@ -33,6 +35,23 @@ heightInput.addEventListener('keyup', ()=>{
     const width = ratioInput.checked ? heightInput.value / ogImageRatio : widthInput.value;
     widthInput.value = Math.floor(width);
 })
-fileInput.addEventListener('change', loadFile);
 
+const resizeAndDownload = ()=>{
+    const canvas = document.createElement('canvas');
+    const a = document.createElement('a');
+    const ctx = canvas.getContext('2d');
+    const imgQuality = qualityInput.checked ? 0.7 : 1.0;
+
+    //mise de la taille et la largeur de l'image à partir de la taille et la largeur de entrees
+
+    canvas.width = widthInput.value;
+    canvas.height = heightInput.value;
+
+    ctx.drawImage(previewImg, 0, 0, canvas.width, canvas.height);
+    a.href = canvas.toDataURL('image/jpeg', imgQuality);
+    a.download = new Date().getTime();
+    a.click();
+}
+downloadBtn.addEventListener('click', resizeAndDownload);
+fileInput.addEventListener('change', loadFile);
 uploadBox.addEventListener('click', ()=> fileInput.click());
