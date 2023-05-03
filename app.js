2ee4c1efeefe6b11558e3b80e6d813334ea98e26
= <=>
2023-05-03 15:11:12 +0200

    {feat: img controls} add image properties

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const uploadBox= document.querySelector('.upload-box'),
 previewImg = uploadBox.querySelector('img'),
-fileInput = uploadBox.querySelector('input');
+fileInput = uploadBox.querySelector('input'),
+widthInput = document.querySelector('.width input'),
+heightInput = document.querySelector('.height input'),
+ratioInput = document.querySelector('.ratio input');
+
+let ogImageRatio;
 
 const loadFile = (e)=>{
     const file = e.target.files[0]; //prendre le premier fichier selectionné
@@ -9,11 +14,25 @@ const loadFile = (e)=>{
 
     previewImg.src = URL.createObjectURL(file); //attribution de l'url de l'image à la variable previewImg
     previewImg.addEventListener('load', ()=>{ //quand l'image est chargé
+        widthInput.value = previewImg.naturalWidth;
+        heightInput.value = previewImg.naturalHeight;
+        ogImageRatio = previewImg.naturalWidth / previewImg.naturalHeight;
         document.querySelector('.wrapper').classList.add('active')
     })
-    console.log(file);
+    
 }
 
+widthInput.addEventListener('keyup', ()=>{
+    //avoir la taille de l'image à partir du ratio si le checkbox est coché
+    const height = ratioInput.checked ? widthInput.value / ogImageRatio : heightInput.value;
+    heightInput.value = Math.floor(height);
+})
+
+heightInput.addEventListener('keyup', ()=>{
+    //avoir la largeur de l'image à partir du ratio si le checkbox est coché
+    const width = ratioInput.checked ? heightInput.value / ogImageRatio : widthInput.value;
+    widthInput.value = Math.floor(width);
+})
 fileInput.addEventListener('change', loadFile);
 
 uploadBox.addEventListener('click', ()=> fileInput.click());
